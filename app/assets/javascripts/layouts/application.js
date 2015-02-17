f228116f4dfc272873a8e58eb8646f22535ba5f6
evg2108 <evg2108@yandex.ru>
2015-02-17 19:54:12 +0700

    add Authors controller with index and show routes and views. create structure of assets

---
+++ app/assets/javascripts/layouts/application.js
@@ -0,0 +1,8 @@
+// This is a manifest file contains declaration requirements of scripts, that used on all pages
+// for "application" layout.
+//
+// This file must be required on any js file for "application" layout
+//
+//= require jquery.js
+//= require jquery_ujs
+//= require bootstrap-sprockets
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_asset_name
+    @current_asset_name ||= "application/#{controller_name}/#{action_name}"
+  end
 end
